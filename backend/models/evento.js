d6f45a369d85c31fd3eae59960f1cdbeeedb7d18
Evandro Florscuk <evandro_ff@me.com>
2022-06-04 16:33:16 -0300

    Adicionar a classe evento

---
+++ backend/models/evento.js
@@ -0,0 +1,14 @@
+class Evento {
+    constructor(id, nome, precoPorIngresso, cidade, estado, pais, dataDoEvento, totalIngressos,
+                ingressosRestantes) {
+        this.id = id
+        this.nome = nome
+        this.precoPorIngresso = precoPorIngresso
+        this.cidade = cidade
+        this.estado = estado
+        this.pais = pais
+        this.dataDoEvento = dataDoEvento,
+        this.totalIngressos = totalIngressos
+        this.ingressosRestantes = ingressosRestantes
+    }
+}
