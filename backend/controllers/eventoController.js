1395e82adc9f9db5e53f8f0ea520ea41769ff2b1
Evandro Florscuk <evandro_ff@me.com>
2022-06-04 17:56:38 -0300

    Definição da função getAllEventos

--- backend/controllers/eventoController.js
+++ backend/controllers/eventoController.js
@@ -4,7 +4,7 @@ const firebase = require('../db')
 const Evento = require('../models/evento')
 const firestore = firebase.firestore()
 
-const addEvento = async (req, res, next) => {
+const addEvento = async(req, res, next) => {
     try {
         const data = req.body
         await firestore.collection('eventos').doc().set(data)
@@ -14,6 +14,38 @@ const addEvento = async (req, res, next) => {
     }
 }
 
+const getAllEventos = async(req, res, next) => {
+    try {
+        const eventos = await firestore.collection('eventos')
+        const data = await eventos.get()
+        const eventosArray = []
+        if(data.empty) {
+            res.status(404).send('No event record found')
+        } else {
+            data.forEach(doc => {
+                const student = new Evento(
+                    doc.id,
+                    doc.data().nome,
+                    doc.data().precoPorIngresso,
+                    doc.data().cidade,
+                    doc.data().estado,
+                    doc.data().pais,
+                    doc.data().diaEvento,
+                    doc.data().mesEvento,
+                    doc.data().anoEvento,
+                    doc.data().totalIngressos,
+                    doc.data().ingressosRestantes
+                )
+                eventosArray.push(evento)
+            })
+            res.send(eventosArray)
+        }
+    } catch(error) {
+        res.status(400).send(error.message)
+    }
+}
+
 module.exports = {
-    addEvento
+    addEvento,
+    getAllEventos
 }
--- backend/models/evento.js
+++ backend/models/evento.js
@@ -14,3 +14,5 @@ class Evento {
         this.ingressosRestantes = ingressosRestantes
     }
 }
+
+module.exports = Evento
