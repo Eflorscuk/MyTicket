04180db2ed4be49a567bfe7236ac570f16e7e4db
Evandro Florscuk <evandro_ff@me.com>
2022-06-04 16:15:02 -0300

    Refatoração do index.js para o correto funcionamento do express server juntamente com as variáveis de ambiente

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,13 @@
+'use strict'
 const express = require('express')
-const index = express()
-const porta = 3001
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const config = require('./config')
 
-index.use(express.json())
+const app = express()
 
-const eventos = ['Festa universitária', 'Festa sertaneja', 'Show de Adoniram']
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.json())
 
-// Retornar um evento
-index.get('/eventos/:index', (req, res) => {
-    const { index } = req.params
-
-    return res.json(eventos[index])
-})
-
-// Retornar todos os eventos
-index.get('/eventos', (req, res) => {
-    return res.json(eventos)
-})
-
-// Criar novo evento
-index.post('/eventos', (req, res) => {
-    const { name } = req.body
-    eventos.push(name)
-
-    return res.json(eventos)
-})
-
-// atualizar evento
-index.put( '/eventos/:index', (req, res) => {
-    const { index } = req.params
-    const { name } = req.body
-
-    eventos[index] = name
-
-    return res.json(eventos)
-})
-
-// Deletar eventos
-index.delete('/eventos/:index', (req, res) => {
-    const { index } = req.params
-
-    eventos.splice(index, 1)
-    return res.json({ message: "O evento foi deletado"})
-})
-
-index.listen(porta, () => console.log(`escutando na porta ${porta}`))
+app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port))
